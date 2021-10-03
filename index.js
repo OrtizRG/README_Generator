2b3eb0d1f5b4ba90c85ccdccea9d675fa02cf162
Reyna <reyna.ortiz84@gmail.com>
2021-10-02 17:15:53 -0700

    writting html to console log from inputs functionality

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const generateMd = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+
+const [name, github] = profileDataArgs;
+
+
 
 // const questions = () =>  {
 //     return inquirer.prompt([
@@ -56,23 +59,32 @@ console.log(profileDataArgs);
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
+const generatePage = (userName, githubName) => {
+    return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+    </head>
   
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-    }
+    <body>
+      <h1>${name}</h1>
+      <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
+    `;
+  };
+
+console.log(generatePage(name, github));
   
 
 // TODO: Create a function to initialize app
 function init() {}
 
-printProfileData(profileDataArgs);
+
 
 // Function call to initialize app
 init();
