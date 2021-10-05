3da7f53cd1e5c49a626f45336614afd9545c8dc5
Reyna <reyna.ortiz84@gmail.com>
2021-10-04 20:00:07 -0700

    added project readme file

---
+++ dist/readme.md
@@ -0,0 +1,35 @@
+# ReadMe Generator
+
+  ## Description
+  Application that generates a readme from the commandline!
+  ## Table of Contents
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [License](#license)
+  
+  ## Installation
+  
+          npm install
+
+  ## Usage
+
+  Run from the command line in order to generate a readme given a template.
+
+  ## Credits
+
+  n/a
+
+  ## License
+  
+  ![License](https://img.shields.io/github/license/Jire/KTON.svg)
+
+  [Learn More about Apache 2.0](https://choosealicense.com/licenses/apache-2.0/)
+
+
+  ## Questions
+
+  Find a link to the full repository at https://github.com/OrtizRG
+
+  For further questions please contact me at [reyna.ortiz84@gmail.com](mailto: reyna.ortiz84@gmail.com)
--- index.js
+++ index.js
@@ -1,95 +1,111 @@
 // TODO: Include packages needed for this application
 
-const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMd = require('./utils/generateMarkdown');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 
 // TODO: Create an array of questions for user input
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const questions =["What is the name of your project?"];
 
-const [name, github] = profileDataArgs;
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is thename of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the project description?'
+      },
 
+      {
+        type: 'input',
+        name: 'Installation',
+        message: 'What applications need to be installed?'
+      },
 
+      {
+        type: 'input',
+        name: 'Usage',
+        message: 'Application (usage) of the project?'
+      },
 
-// const questions = () =>  {
-//     return inquirer.prompt([
-//       {
-//         type: 'input',
-//         name: 'name',
-//         message: 'What is your project title?',
-//         validate: nameInput => {
-//           if (nameInput) {
-//             return true;
-//           } else {
-//             console.log('Please enter your project title');
-//             return false;
-//           }
-//         }
-//       },
-//       {
-//         type: 'input',
-//         name: 'github',
-//         message: 'Enter your GitHub Username (Required)',
-//         validate: githubInput => {
-//           if (githubInput) {
-//             return true;
-//           } else {
-//             console.log('Please enter your GitHub username!');
-//             return false;
-//           }
-//         }
-//       },
-//       {
-//         type: 'confirm',
-//         name: 'confirmAbout',
-//         message: 'Would you like to enter some information about yourself for an "About" section?',
-//         default: true
-//       },
-//       {
-//         type: 'input',
-//         name: 'about',
-//         message: 'Provide some information about yourself:',
-//         when: ({ confirmAbout }) => confirmAbout
-//       }
-//     ]);
-//   };
+    {
+      type: 'list',
+      message: 'What license?',
+      name: 'License',
+      choices: ['MIT', 'Apache', 'MPL', 'GPL']
+    },
+    {
+      type: 'checkbox',
+      message: 'What languages do you know?',
+      name: 'stack',
+      choices: ['HTML', 'CSS', 'JavaScript', 'SQL']
+    }
+  ])
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-const generatePage = (userName, githubName) => {
-    return `
-    <!DOCTYPE html> 
-    <html lang="en"> 
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Portfolio Demo</title>
-    </head>
-  
-    <body>
-      <h1>${name}</h1>
-      <h2><a href="https://github.com/${github}">Github</a></h2>
-    </body>
-    </html>
-    `;
-  };
+let data = "Learning how to write in a file."
 
-fs.writeFile('index.md', generatePage(name, github), err => {
-    if (err) throw err;
+function writeToFile(fileName, data) {
+    fs.writeFile('README.md', data, err => {
+        if (err) throw err;
+     
+})
+};
 
-    console.log('Porfolio complete!')
+// const generatePage = (userName, githubName) => {
+//     return `
+
+//     ##Description 
+//     ${userName}
+//     ${githubName}
+
+//     ## Table of Contents
+
+//     * [Installation](#installation)
+//     * [Usage](#usage)
+//     * [Credits](#credits)
+//     * [License](#license)
     
-});
+//     ## Installation
+
+//     What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+    
+    
+//     ## Usage 
+    
+//     Provide instructions and examples for use. Include screenshots as needed. 
+    
+//     ## License
+    
+//     The last section of a good README is a license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, use [https://choosealicense.com/](https://choosealicense.com/)
+    
+//     ## Tests
+
+//     ## Questions
+//     `;
+//   };
+
+// fs.writeFile('README.md', generatePage(name, github), err => {
+//     if (err) throw err;
+
+//     console.log('Porfolio complete!')
+    
+// });
   
 
 // TODO: Create a function to initialize app
-function init() {}
+
+
 
 
 
 // Function call to initialize app
-init();
+    
+init (writeToFile);
